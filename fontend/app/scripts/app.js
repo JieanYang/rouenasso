755915ea6d45c99fc4ab19873d686db9fc1e4644
DESKTOP-E8OLI53\yang <yja8534786>
2017-09-21 23:06:14 +0200

    删除vue.js创建的前端文件夹，加入angularjs的前端文件夹，在document里加入一个正式的前端说明

---
+++ fontend/app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('frontendApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                        controller  : 'HomeController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
+                }
+
+            });
+        
+           
+    
+        $urlRouterProvider.otherwise('/');
+    })
+;
