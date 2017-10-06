67e030b4a6c041322f740caf28da3b8f55b848b8
HE Junyang <nathanhejunyang@gmail.com>
2017-10-06 14:15:30 +0200

    创建枚举基类，Department枚举类，Position枚举类，完成Users类。 更新数据库Migration和Seeder

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Model\Position;
+use App\Model\Department;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+        	'name' => '小何',
+        	'email' => 'xiaohe@test.com',
+        	'password' => bcrypt('he'),
+        	'department' => Department::ZHUXITUAN,
+        	'position' => Position::ZHUXI,
+        	'school' => 'ESIGELEC',
+        	'phone_number' => '06 05 04 03 02',
+        	'isWorking' => True,
+        	'isAvaible' => True,
+        	'birthday' => date('1993-01-01'),
+        	'arrive_date' => date('2012-01-01'),
+        	'dimission_date' => date('2018-01-01'),
+        	'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('users')->insert([
+        	'name' => '小宋',
+        	'email' => 'xiaosong@test.com',
+        	'password' => bcrypt('song'),
+        	'department' => Department::ZHUXITUAN,
+        	'position' => Position::FUZHUXI,
+        	'school' => 'ESIGELEC',
+        	'phone_number' => '06 05 04 03 01',
+        	'isWorking' => True,
+        	'isAvaible' => True,
+        	'birthday' => date('1993-01-02'),
+        	'arrive_date' => date('2012-01-02'),
+        	'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
     }
 }
