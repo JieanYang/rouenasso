683a78b9cd15216213ce918e647ecb0947d54f5c
jiean yang <yang.jiean14@gmail.com>
2017-10-31 14:05:40 +0100

    注册页满足基本要求，需要优化用户体验和美观

--- frontend/src/scripts/scripts_Module/register.js
+++ frontend/src/scripts/scripts_Module/register.js
@@ -5,16 +5,46 @@ module.exports = {
 };
 
 /** @ngInject */
-function registerController() {
+function registerController($http) {
   var vm = this;
-//   $http
-// .get('http://localhost:8000/test')
-// .then(function (response) {
-//   vm.contents = response.data;
-//   $log.log(vm.contents);
-// });
-  var submit = function ($log) {
-    $log.log('a');
+  //for the tag selection ng-options
+  vm.department_channels=[{label:"项目开发部", value:"项目开发部"}, 
+  						  {label:"组织部", value:"组织部"},
+  						  {label:"安全部", value:"安全部"}];
+  vm.positions = [{label:"成员", value:"成员"}];
+
+  vm.birthday;//input birthday
+  vm.arrive_date;//input arrive_date
+  
+  //initialize information of register
+  // vm.register = {"name" : "",
+		// 		"email" : "",
+		// 		"department":"",
+	 //    		"position": "",
+		// 		"school": "",
+		// 		"phone_number": "",
+		// 		"birthday": "",
+		// 		"arrive_date": "",
+		// 		"password": ""};
+  vm.sendRegister = function () {
+  	vm.register.birthday = vm.birthday.toLocaleDateString('zh-Hans-CN');
+  	vm.register.arrive_date = vm.arrive_date.toLocaleDateString('zh-Hans-CN');
+
+  	console.log(vm.register);
+
+  	$http.post('http://localhost:8000/register', vm.register, true)
+  	.then(function(response) {
+  		if(response.data.msg){
+  			vm.message = response.data.msg;
+  		console.log(vm.message);
+  		}
+  		if(response.data.email){
+  			vm.message = response.data.email;
+  			console.log(vm.message);
+  		}
+  		
+  	},function(){
+  		console.log("It meet a error!");
+  	});
   };
-  vm.submit = submit;
 }
