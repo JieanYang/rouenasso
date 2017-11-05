b5b6470f2fc55a07b103807264f70319da17e254
Junyang HE <nathanhejunyang@gmail.com>
2017-11-06 03:12:54 +0800

    Fix front index announcement issue

--- frontend/src/scripts/scripts_Module/contents.js
+++ frontend/src/scripts/scripts_Module/contents.js
@@ -17,6 +17,6 @@ function ContentsController($http, $sce) {
   $http
 .get('//api.acecrouen.com/posts/category/99?latest=true')
 .then(function (response) {
-  vm.announcement = $sce.trustAsHtml(response.data[0].html_content);
+  vm.announcement = $sce.trustAsHtml(response.data.html_content);
 });
 }
