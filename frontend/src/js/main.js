d83f9895c737a260baa969287bd6eed4c81a4423
Junyang HE <nathanhejunyang@gmail.com>
2017-11-18 13:15:09 +0100

    重新显示下拉状态的导航栏，添加平滑scroll动画，添加scroll offset使导航栏不会遮挡跳转目标

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -3,12 +3,9 @@ module.exports = {
   initSmallHeader: initSmallHeader
 };
 
-console.log("loaded");
-
 var owlPricing;
 var ratio = 2;
 
-// Window Load
 function initBigHeader() {
     // Preloader
     $('.intro-tables, .parallax, header').css('opacity', '0');
@@ -63,6 +60,15 @@ function initSmallHeader() {
     $('nav.navbar-fixed-top').css('opacity', '0');
 
     $('#placeHolder').height(160).css('background-color', '#000147');
+
+	// smooth anchor scroll
+	$('html, body').on('click', 'a[href^="#"]', function (event) {
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top - 110
+		}, 500);
+	});
 };
 
 $(window).load(function () {
@@ -112,8 +118,8 @@ function onScroll() {
     }
 }
 
-// window.addEventListener('scroll', onScroll, false);
-
+window.addEventListener('scroll', onScroll, false);
+	
 // Window Resize
 $(window).resize(function () {
     $('header').height($(window).height());
@@ -151,51 +157,3 @@ $('body').on('click', '.mobile-nav a', function (event) {
         }
     }
 });
-
-/*
-$('body').on('click', '.mobile-nav a.close-link', function (event) {
-    $('.mobile-nav').removeClass('active');
-    event.preventDefault();
-});
-
-
-$('body').on('click', 'nav.original .navbar-nav a:not([data-toggle])', function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        event.stopPropagation();
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
-*/
-
-/*
-function centerModal() {
-    $(this).css('display', 'block');
-    var $dialog = $(this).find(".modal-dialog"),
-        offset = ($(window).height() - $dialog.height()) / 2,
-        bottomMargin = parseInt($dialog.css('marginBottom'), 10);
-
-    // Make sure you don't hide the top part of the modal w/ a negative margin
-    // if it's longer than the screen height, and keep the margin equal to
-    // the bottom margin of the modal
-    if (offset < bottomMargin) offset = bottomMargin;
-    $dialog.css("margin-top", offset);
-}
-
-$('.modal').on('show.bs.modal', centerModal);
-
-$('.modal-popup .close-link').click(function (event) {
-    event.preventDefault();
-    $('#modal1').modal('hide');
-});
-
-$(window).on("resize", function () {
-    $('.modal:visible').each(centerModal);
-});
-*/
