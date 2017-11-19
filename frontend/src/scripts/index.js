8211058fedefe0cfd3b1cdd5a8da7607a5d77fe2
Yang <bboylevi89@gmail.com>
2017-11-19 18:47:22 +0100

    学联关于我们页面完成

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -7,7 +7,7 @@ var movement = require('./scripts_Module/movement');
 var work = require('./scripts_Module/work');
 var writing = require('./scripts_Module/writing');
 var contactus = require('./scripts_Module/contactus');
-
+var aboutus = require('./scripts_Module/aboutus');
 
 var register = require('./scripts_Module/register');
 var login = require('./scripts_Module/loginSample');
@@ -25,8 +25,6 @@ angular
   .component('work', work)
   .component('writing', writing)
   .component('contactus', contactus)
-
+  .component('aboutus',aboutus)
   .component('register', register)
   .component('login', login);
-
-
