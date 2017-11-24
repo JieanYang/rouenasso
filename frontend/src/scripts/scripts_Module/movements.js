5c34ecebf20d2e4427359f8248844ecb87120327
jiean yang <yja8534786@gmail.com>
2017-11-24 18:31:41 +0100

    优化活动推广详情页面内容；活动推广目录页面连接后端得到数据

--- frontend/src/scripts/scripts_Module/movements.js
+++ frontend/src/scripts/scripts_Module/movements.js
@@ -8,11 +8,10 @@ module.exports = {
 function movementsController($http) {
   var vm = this;
 
-  $http.get('https://api.acecrouen.com//posts/category/1')//活动类型
+  $http.get('http://localhost:8000/movements')
   .then(function(response){
   	vm.data = response.data;
-  	vm.id=0;
 
-  	console.log(vm.data)
+  	// console.log(vm.data)
   });
 }
