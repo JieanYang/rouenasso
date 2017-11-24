8a7685d125c6dd4bc0f7fb3dc5cdf510a623001f
jiean yang <yja8534786@gmail.com>
2017-11-24 13:26:36 +0100

    优化联系我们页面

--- frontend/src/scripts/scripts_Module/contactus.js
+++ frontend/src/scripts/scripts_Module/contactus.js
@@ -5,9 +5,39 @@ module.exports = {
 };
 
 /** @ngInject */
-function contactusController() {
-  var vm = this;
-  vm.channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+function contactusController($http) {
+	var vm = this;
+	vm.message = "您的留言已经提交到学联处！";
+	vm.showMessage=false;
+	vm.channels = [{value:"phone", label:"Tel."}, {value:"email",label:"Email"}];
 
-  vm.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+  // vm.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+	vm.feedback = {name_leaveMessage:"", phone_leaveMessage:"", email_leaveMessage:"",
+   agreeContact_leaveMessage:false, contactWay_leaveMessage:"", message_leaveMessage:""};
+
+   // vm.feedback = {name_leaveMessage:"杜甫", phone_leaveMessage:"0123456789", email_leaveMessage:"dufu@test.com",
+   // agreeContact_leaveMessage:false, contactWay_leaveMessage:"", message_leaveMessage:"我是李白大人的兄弟，敢动他就是在我头上动土！"};
+
+
+   // console.log(feedback.reset);
+   // vm.a = function (){
+   // 	vm.feedback = {name_leaveMessage:"", phone_leaveMessage:"", email_leaveMessage:"",
+   // agreeContact_leaveMessage:false, contactWay_leaveMessage:"", message_leaveMessage:""};
+   // 	  feedbackForm.$setPristine();????
+   // }
+
+ 
+	vm.sendFeedback = function() {
+		
+		$http.post("http://localhost:8000/leaveMessages", vm.feedback)
+		.then(function (response) {
+			console.log(response);
+			if(response.data.msg == "success") {
+					vm.feedback = {name_leaveMessage:"", phone_leaveMessage:"", email_leaveMessage:"",
+								   agreeContact_leaveMessage:false, contactWay_leaveMessage:"", message_leaveMessage:""};
+					vm.showMessage=true;	
+			}
+		});
+	}
+
 }
