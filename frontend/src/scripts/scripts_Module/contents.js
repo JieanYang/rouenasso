d5a8df61cbc11a2a7604df5fd96d220b9dfd6653
jiean yang <yang.jiean14@gmail.com>
2017-12-17 17:56:52 +0100

    前端公告会根据前端后台设置显示

--- frontend/src/scripts/scripts_Module/contents.js
+++ frontend/src/scripts/scripts_Module/contents.js
@@ -1,17 +1,24 @@
 module.exports = {
   template: require('../../views/views_Module/contents.html'),
   controller: ContentsController,
-  controllerAs: 'controller'
+  controllerAs: 'vm'
 };
 
 /** @ngInject */
 function ContentsController($http, $sce) {
 	var vm = this;
+	vm.showAnnouncement = false;
 
 	$http
 	.get('https://api.acecrouen.com/posts/category/99?latest=true')
 	.then(function (response) {
-	vm.announcement = $sce.trustAsHtml(response.data.html_content);
+		if (response.data.preview_text == 'hiding'){
+			vm.showAnnouncement = false;
+		}else if (response.data.preview_text == 'showing'){
+			vm.announcement = $sce.trustAsHtml(response.data.html_content);
+			vm.showAnnouncement = true;
+		}
+
 	});
 
 	$http.get('https://api.acecrouen.com/log');
