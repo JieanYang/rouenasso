80d2960f2badd24b4f6c66ce68d6201bc8cd270e
jiean yang <yang.jiean14@gmail.com>
2017-12-17 15:46:03 +0100

    首页计数

--- frontend/src/scripts/scripts_Module/contents.js
+++ frontend/src/scripts/scripts_Module/contents.js
@@ -6,13 +6,13 @@ module.exports = {
 
 /** @ngInject */
 function ContentsController($http, $sce) {
-  var vm = this;
+	var vm = this;
 
+	$http
+	.get('https://api.acecrouen.com/posts/category/99?latest=true')
+	.then(function (response) {
+	vm.announcement = $sce.trustAsHtml(response.data.html_content);
+	});
 
-
-  $http
-.get('https://api.acecrouen.com/posts/category/99?latest=true')
-.then(function (response) {
-  vm.announcement = $sce.trustAsHtml(response.data.html_content);
-});
+	$http.get('https://api.acecrouen.com/log');
 }
