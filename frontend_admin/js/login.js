ce54bbfd6618190dc327cd26aaaa23e95aa69f0b
HE Junyang <nathanhejunyang@gmail.com>
2017-12-24 16:51:08 +0100

    前端后台 添加登陆职位限制

--- frontend_admin/js/login.js
+++ frontend_admin/js/login.js
@@ -20,17 +20,24 @@ function attemptLogin() {
     $("#btn-submit").prop('disabled', true);
 
     attempt(auth, function (response) {
-        $("#hint").text("欢迎，" + response.name);
-        $("#hint").css('color', 'green');
-        $("#hint").show();
-        $("#loader").removeClass("show");
+        if(response.department == "主席团" || response.department == "秘书部" || response.department == "宣传部"){
+	        $("#hint").text("欢迎，" + response.name);
+	        $("#hint").css('color', 'green');
+	        $("#hint").show();
+	        $("#loader").removeClass("show");
 
-        setAuthCookie(auth);
-
-        setTimeout(
-            function () {
-                window.location.replace("index.html");
-            }, 1500);
+        	setAuthCookie(auth);
+	        setTimeout(
+	            function () {
+	                window.location.replace("index.html");
+	            }, 1500);
+        } else {
+	        $("#hint").text("你无权限登陆");
+	        $("#hint").css('color', 'red');
+	        $("#hint").show();
+	        $("#btn-submit").prop('disabled', false);
+	        $("#loader").removeveClass("show");
+        }
     }, function (response) {
         $("#hint").text("邮箱或密码错误");
         $("#hint").css('color', 'red');
